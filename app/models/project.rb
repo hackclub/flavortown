761370099eaa11b72186bb5a29fd25b526bd0862
Kartikey Chauhan <me@cskartikey.dev>
2025-10-19 00:20:53 +0530

    feat (project): add validations for banner and preprocess variants

--- app/models/project.rb
+++ app/models/project.rb
@@ -17,8 +17,34 @@ class Project < ApplicationRecord
     has_many :users, through: :memberships
 
     has_one_attached :demo_video
-    has_one_attached :banner
+    # https://github.com/rails/rails/pull/39135
+    has_one_attached :banner do |attachable|
+        # using resize_to_fill instead of resize_to_limit because consistency. might change it to resize_to_limit
+        # we're preprocessing them because its likely going to be used
+
+        # for explore and projects#index
+        attachable.variant :card,
+                           resize_to_fill: [ 1600, 900 ],
+                           format: :webp,
+                           preprocess: true,
+                           saver: { strip: true, quality: 75 }
+
+        #   attachable.variant :not_sure,
+        #     resize_to_fill: [ 1200, 630 ],
+        #     format: :webp,
+        #     saver: { strip: true, quality: 75 }
+
+        # for voting
+        attachable.variant :thumb,
+                           resize_to_fill: [ 400, 210 ],
+                           format: :webp,
+                           preprocess: true,
+                           saver: { strip: true, quality: 75 }
+    end
 
     validates :title, presence: true, length: { maximum: 120 }
     validates :description, length: { maximum: 1_000 }, allow_blank: true
+    validates :banner,
+              content_type: [ "image/jpeg", "image/png", "image/webp", "image/heic", "image/heif" ],
+              size: { less_than: 10.megabytes, message: "is too large (max 10 MB)" }
 end
