12a67d382e751335516a1578bf4a2e7061c8d7c3
Neon <neon@saahild.com>
2025-10-20 00:30:54 +0000

    update: touch up dash + fix ui and policy

--- app/policies/admin_policy.rb
+++ app/policies/admin_policy.rb
@@ -1,14 +1,14 @@
 class AdminPolicy < ApplicationPolicy
     def blazer?
-      user.try(:is_admin)
+      user.try(:admin?)
     end
   
     def flipper?
-      user.try(:is_admin)
+      user.try(:admin?)
     end
   
     def access_admin_endpoints?
-      user.try(:is_admin) || user.try(:is_fraud_dept)
+      user.try(:admin?) || user.try(:fraud_dept)
     end
   end
   
