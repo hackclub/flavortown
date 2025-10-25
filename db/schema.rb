e7be9acbdcef4134a90ffb6b5a9ec952247d4cf7
Max Wofford <max@maxwofford.com>
2025-10-25 12:59:44 -0400

    add rsvp stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_20_125311) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_25_160950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -161,6 +161,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_20_125311) do
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rsvps", force: :cascade do |t|
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
