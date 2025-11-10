821cde62f9f6708f24ed7a73248a111b362e3936
Kartikey Chauhan <me@cskartikey.dev>
2025-11-10 23:40:41 +0530

    feat: add dropdown

--- app/components/input_component.rb
+++ app/components/input_component.rb
@@ -3,9 +3,9 @@
 class InputComponent < ViewComponent::Base
   COLORS = %i[red blue green yellow].freeze
 
-  attr_reader :label, :placeholder, :color, :subtitle, :form, :attribute
+  attr_reader :label, :placeholder, :color, :subtitle, :form, :attribute, :collection
 
-  def initialize(label:, placeholder:, form:, attribute:, color: :yellow, subtitle: nil, as: :text_field, input_html: {})
+  def initialize(label:, placeholder:, form:, attribute:, color: :yellow, subtitle: nil, as: :text_field, input_html: {}, collection: nil, select_options: {})
     @label = label
     @placeholder = placeholder
     @form = form
@@ -14,6 +14,10 @@ class InputComponent < ViewComponent::Base
     @subtitle = subtitle
     @field_method = normalize_field_method(as)
     @input_html = input_html.to_h
+    @collection = collection
+    @select_options = select_options.to_h
+
+    validate_configuration!
   end
 
   def input_classes
@@ -21,7 +25,12 @@ class InputComponent < ViewComponent::Base
   end
 
   def field_tag
-    form.public_send(field_method, attribute, field_options)
+    case field_method
+    when :select
+      form.select(attribute, collection, select_field_options, field_options)
+    else
+      form.public_send(field_method, attribute, field_options)
+    end
   end
 
   def has_subtitle?
@@ -30,12 +39,12 @@ class InputComponent < ViewComponent::Base
 
   private
 
-  attr_reader :field_method, :input_html
+  attr_reader :field_method, :input_html, :select_options
 
   def field_options
     options = input_html.dup
-    options[:placeholder] ||= placeholder
-    options[:class] = class_names("input__field", ("input__field--textarea" if field_method == :text_area), options[:class])
+    options[:placeholder] ||= placeholder if apply_placeholder_to_field?
+    options[:class] = class_names("input__field", field_modifier_class, options[:class])
     options[:rows] ||= 5 if field_method == :text_area
     options
   end
@@ -50,8 +59,36 @@ class InputComponent < ViewComponent::Base
   def normalize_field_method(value)
     method = value.to_sym
     return :text_area if method == :textarea
+    return :select if method == :select
     return method if %i[text_field text_area].include?(method)
 
     raise ArgumentError, "Unsupported field type #{value.inspect}"
+  end
+
+  def apply_placeholder_to_field?
+    field_method != :select
+  end
+
+  def field_modifier_class
+    case field_method
+    when :text_area
+      "input__field--textarea"
+    when :select
+      "input__field--select"
+    end
+  end
+
+  def select_field_options
+    return {} unless field_method == :select
+
+    options = select_options.dup
+    options[:prompt] = placeholder if placeholder.present? && !options.key?(:prompt) && !options.key?(:include_blank)
+    options
+  end
+
+  def validate_configuration!
+    return unless field_method == :select
+
+    raise ArgumentError, "collection is required for select inputs" if collection.nil?
   end
 end
