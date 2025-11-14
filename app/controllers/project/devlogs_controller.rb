a3d81bbbf606846a4bfe0ce515e4b26fa0a3d3f0
Kartikey Chauhan <me@cskartikey.dev>
2025-11-14 21:04:12 +0530

    feat: add new devlog view

---
+++ app/controllers/project/devlogs_controller.rb
@@ -0,0 +1,32 @@
+class Project::DevlogsController < ApplicationController
+  before_action :set_project
+
+  def new
+    @devlog = Post::Devlog.new
+  end
+
+  def create
+    @devlog = Post::Devlog.new(devlog_params)
+
+    if @devlog.save
+      Post.create!(project: @project, user: current_user, postable: @devlog)
+      flash[:notice] = "Devlog created successfully"
+      redirect_to @project
+    else
+      flash.now[:alert] = @devlog.errors.full_messages.to_sentence
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def devlog_params
+    params.require(:post_devlog).permit(:body, attachments: [])
+  end
+end
+
+
