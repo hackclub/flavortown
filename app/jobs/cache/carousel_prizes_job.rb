4577717000ed2025b6eacec2832c4c9febe0f747
Kartikey Chauhan <skartikey314@gmail.com>
2025-11-17 22:47:19 +0530

    Revert "Add carousel cache invalidation + better loading (#124)" (#125)
    This reverts commit 55b18e726d079ef9da391c9003efc861ae928839.

--- app/jobs/cache/carousel_prizes_job.rb
+++ app/jobs/cache/carousel_prizes_job.rb
@@ -1,6 +1,4 @@
 class Cache::CarouselPrizesJob < ApplicationJob
-  include Rails.application.routes.url_helpers
-
   queue_as :literally_whenever
 
   CACHE_KEY = "landing_carousel_prizes"
@@ -12,21 +10,11 @@ class Cache::CarouselPrizesJob < ApplicationJob
               .shown_in_carousel
               .order(:ticket_cost)
               .map do |prize|
-        urls = if prize.image.attached?
-          {
-            sm: rails_blob_url(prize.image.variant(:carousel_sm), expires_in: CACHE_DURATION + 10.seconds),
-            md: rails_blob_url(prize.image.variant(:carousel_md), expires_in: CACHE_DURATION + 10.seconds),
-            lg: rails_blob_url(prize.image.variant(:carousel_lg), expires_in: CACHE_DURATION + 10.seconds),
-            original: rails_blob_url(prize.image, expires_in: CACHE_DURATION + 10.seconds)
-          }
-        end
-
         {
           id: prize.id,
           name: prize.name,
           hours_estimated: prize.hours_estimated,
-          image_url: urls&.[](:md),
-          srcset: urls && "#{urls[:sm]} 160w, #{urls[:md]} 240w, #{urls[:lg]} 360w, #{urls[:original]} 800w"
+          has_image: prize.image.attached?
         }
       end
     end
