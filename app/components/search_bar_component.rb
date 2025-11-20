0dbca6e54c5146f5835791eab93bde742bf15a6c
Shreyas Deo <93237883+deoshreyas@users.noreply.github.com>
2025-11-20 19:43:12 +0000

    Fix basic shop page styles

--- app/components/search_bar_component.rb
+++
@@ -1,9 +0,0 @@
-class SearchBarComponent < ViewComponent::Base
-  def initialize(placeholder: "Search item", q: nil, action: nil)
-    @placeholder = placeholder
-    @q = q
-    @action = action || "/search"
-  end
-
-  attr_reader :placeholder, :q, :action
-end
