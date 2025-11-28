9398540b52ca8d042d4efa927032a63843af013f
Amber <amber@transcental.dev>
2025-11-28 17:42:23 +0000

    Fix indentation &  grant matching regex (#182)
    * fix indentation
    
    * fix grant code matching regex on urls in forwarded emails

--- app/mailboxes/hcb_mailbox.rb
+++ app/mailboxes/hcb_mailbox.rb
@@ -2,7 +2,7 @@ class HCBMailbox < ApplicationMailbox
   def process
     if mail.subject.include?("You've received a donation for Flavortown")
       if mail.body.decoded =~ %r{grants/}
-        grant_id = mail.body.decoded.split("grants/").last.to_s.split(/[\/\s]/).first
+        grant_id = mail.body.decoded.split("grants/").last.to_s.split(/[^a-zA-Z0-9]/).first
         shop_card_grant = ShopCardGrant.find_by(hcb_grant_hashid: "cdg_" + grant_id)
         donation_id = mail.body.decoded.split("donations/").last.to_s.split(/[\/\s]/).first.remove('"')
 
