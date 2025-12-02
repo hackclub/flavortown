fc4e2af69af05398829dd950eb900961b7006487
Neon <neon@saahild.com>
2025-12-02 18:20:31 +0000

    fix: remove schema changes + change to an if statement

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,7 @@
 Rails.application.config.billy_url = (Rails.application.credentials.dig(:constants, :billy_url) || ENV["BILLY_URL"]) || "https://billy/"
 Rails.application.config.joe_url = (Rails.application.credentials.dig(:constants, :joe_url) || ENV["JOE_URL"]) || "https://joe/"
-Rails.application.config.identity = (Rails.application.credentials.dig(:hack_club, :site) || Rails.application.credentials.dig(:constants, :identity_url) || ENV["IDV_URL"]) || "https://auth.hackclub.com/"
+Rails.application.config.identity = if Rails.env.production?
+  "https://auth.hackclub.com"
+else
+  "https://hca.dinosaurbbq.org"
+end
