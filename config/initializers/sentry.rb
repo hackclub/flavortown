66f3cea51f74b99b561192dceb7b9257fd9979e2
Echo <github@3kh0.net>
2025-12-02 14:10:57 -0500

    slim down sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,26 +1,14 @@
-Sentry.init do |config|
-  config.dsn = Rails.application.credentials.dig(:sentry, :dsn) || ENV["SENTRY_DSN"]
-  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+# frozen_string_literal: true
 
-  # Add data like request headers and IP for users,
-  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+Sentry.init do |config|
+  config.dsn = ENV["SENTRY_DSN"]
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
   config.send_default_pii = true
+  config.traces_sample_rate = 1.0
+  config.profiles_sample_rate = 1.0
 
   # Enable sending logs to Sentry
   config.enable_logs = true
   # Patch Ruby logger to forward logs
   config.enabled_patches = [ :logger ]
-
-  # Set traces_sample_rate to 1.0 to capture 100%
-  # of transactions for tracing.
-  # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
-  # or
-  config.traces_sampler = lambda do |context|
-    true
-  end
-  # Set profiles_sample_rate to profile 100%
-  # of sampled transactions.
-  # We recommend adjusting this value in production.
-  config.profiles_sample_rate = 1.0
 end
