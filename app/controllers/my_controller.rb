ffd175712c40ff832f36c35f839aafb2cc414ffe
Neon <neon@saahild.com>
2025-12-02 15:50:32 -0500

    Fix votes (#209)

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,5 +1,13 @@
 class MyController < ApplicationController
+  before_action :require_login
+
   def balance
-    @balance = current_user.ledger_entries.all
+    @balance = current_user.ledger_entries.order(created_at: :desc)
+  end
+
+  private
+
+  def require_login
+    redirect_to root_path, alert: "Please log in first" and return unless current_user
   end
 end
