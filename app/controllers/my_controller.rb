7d033d756a0e76f7c043d9aca0156b490f2028b2
Shreyas Deo <93237883+deoshreyas@users.noreply.github.com>
2025-12-03 22:27:49 +0530

    Add modal to view user transaction ledger (#224)
    * Make transaction ledger a modal
    
    * Linting
    
    * Small fix in voting page

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -2,6 +2,11 @@ class MyController < ApplicationController
   before_action :require_login
 
   def balance
+    unless turbo_frame_request?
+      redirect_to root_path
+      return
+    end
+
     @balance = current_user.ledger_entries.order(created_at: :desc)
   end
 
