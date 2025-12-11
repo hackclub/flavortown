ef64bd14ae13e7501aa53d33dea2d1868482eace
Dhamari Trice-Hanson <39872667+dhamariT@users.noreply.github.com>
2025-12-11 17:07:10 -0500

    Add project type categorization for self-classification

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -175,7 +175,7 @@ class ProjectsController < ApplicationController
   end
 
   def project_params
-    params.require(:project).permit(:title, :description, :demo_url, :repo_url, :readme_url, :banner)
+    params.require(:project).permit(:title, :description, :demo_url, :repo_url, :readme_url, :banner, :project_type)
   end
 
   def hackatime_project_ids
