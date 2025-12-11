a230fa3491347c8a5591fa620445c368e759fc4a
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-10 22:09:44 -0500

    add Achievementable concern

---
+++ app/controllers/concerns/achievementable.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Achievementable
+  extend ActiveSupport::Concern
+
+  included do
+    class_attribute :achievements_to_check, default: []
+  end
+
+  class_methods do
+    def grant_earned_achievements(*slugs, **options)
+      self.achievements_to_check = slugs.map(&:to_sym)
+      after_action :check_and_grant_earned_achievements, **options
+    end
+  end
+
+  private
+
+  def grant_achievement!(slug)
+    return unless current_user
+
+    current_user.award_achievement!(slug)
+  end
+
+  def check_and_grant_earned_achievements
+    return unless current_user
+
+    achievements_to_check.each do |slug|
+      grant_achievement!(slug)
+    end
+  end
+end
