008763ed2342fe95380e25af32661337d8418a8f
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-10 22:12:33 -0500

    return achievement if achievement

--- app/models/user.rb
+++ app/models/user.rb
@@ -215,12 +215,13 @@ class User < ApplicationRecord
   end
 
   def award_achievement!(slug)
-    return if earned_achievement?(slug)
+    return nil if earned_achievement?(slug)
 
     achievement = ::Achievement.find(slug)
-    return unless achievement.earned_by?(self)
+    return nil unless achievement.earned_by?(self)
 
     achievements.create!(achievement_slug: slug.to_s, earned_at: Time.current)
+    achievement
   end
 
   def check_and_award_achievements!
