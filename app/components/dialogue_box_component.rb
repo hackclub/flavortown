9e3f4ee1d8c49053236cd2ca0733f9be3dc22a9e
Shreyas Deo <93237883+deoshreyas@users.noreply.github.com>
2025-12-12 13:33:16 +0530

    linting

--- app/components/dialogue_box_component.rb
+++ app/components/dialogue_box_component.rb
@@ -2,6 +2,6 @@ class DialogueBoxComponent < ViewComponent::Base
   attr_reader :text
 
   def initialize(text:, button_text: "Continue")
-    @text = text.is_a?(Array) ? text : [text]
+    @text = text.is_a?(Array) ? text : [ text ]
   end
 end
