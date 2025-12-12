8c29e5d293c56a5fe5f02cc3214262f142abc7cb
Neon <neon@saahild.com>
2025-12-12 04:15:39 +0000

    fix: sentry addr sheningaains smt

--- app/jobs/project/magic_happening_letter_job.rb
+++ app/jobs/project/magic_happening_letter_job.rb
@@ -3,13 +3,16 @@ class Project::MagicHappeningLetterJob < ApplicationJob
 
   def perform(project)
     owner = project.memberships.owner.first&.user
-    return unless owner&.frozen_address
+    return unless owner
+
+    address = owner.addresses.first
+    return unless address.present?
 
     response = TheseusService.create_letter_v1(
       "instant/flavortown-magic-happening",
       {
         recipient_email: owner.email,
-        address: owner.frozen_address,
+        address: address,
         idempotency_key: "flavortown_magic_project_#{project.id}",
         metadata: {
           flavortown_user: owner.id,
