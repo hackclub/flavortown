8093cae4b185bafee2178a9b82ba8f1b11e4dfea
Neon <neon@saahild.com>
2025-12-13 21:42:21 +0000

    stinky annotations

--- app/models/post/ship_event.rb
+++ app/models/post/ship_event.rb
@@ -2,14 +2,17 @@
 #
 # Table name: post_ship_events
 #
-#  id          :bigint           not null, primary key
-#  body        :string
-#  hours       :float
-#  multiplier  :float
-#  payout      :float
-#  votes_count :integer          default(0), not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id                   :bigint           not null, primary key
+#  body                 :string
+#  certification_status :string           default("pending")
+#  feedback_reason      :text
+#  feedback_video_url   :string
+#  hours                :float
+#  multiplier           :float
+#  payout               :float
+#  votes_count          :integer          default(0), not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
 #
 class Post::ShipEvent < ApplicationRecord
   include Postable
