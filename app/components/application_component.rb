594f52372b164e1a838d7de174494308cd56985e
Max Wofford <max@maxwofford.com>
2025-12-12 19:42:30 -0500

    Address annoying phlex warning in development

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,5 +1,14 @@
 class ApplicationComponent < Phlex::HTML
   include Phlex::Rails::Helpers::Routes
+  include Phlex::Rails::Helpers::AssetPath
+  include Phlex::Rails::Helpers::ImageTag
+  include Phlex::Rails::Helpers::URLFor
+  include Phlex::Rails::Helpers::TimeAgoInWords
+  include Phlex::Rails::Helpers::DistanceOfTimeInWords
+
+  register_output_helper :inline_svg_tag
+  register_value_helper :policy
+  register_output_helper :md
 
   if Rails.env.development?
     def before_template
