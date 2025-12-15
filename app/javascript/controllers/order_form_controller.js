2c8f781245f497c0282d9e9d6ae9adbf33033eba
Echo <github@3kh0.net>
2025-12-15 17:57:27 -0500

    tidy up shop ui

--- app/javascript/controllers/order_form_controller.js
+++ app/javascript/controllers/order_form_controller.js
@@ -8,5 +8,27 @@ export default class extends Controller {
     if (!this.hasAddressesValue) {
       this.submitButtonTarget.disabled = true;
     }
+    this.undo();
+  }
+
+  undo() {
+    const radios = this.element.querySelectorAll(
+      ".shop-order__accessory-option-input"
+    );
+    radios.forEach((radio) => {
+      radio.addEventListener("click", (e) => {
+        if (radio.dataset.wasChecked === "true") {
+          radio.checked = false;
+          radio.dataset.wasChecked = "false";
+          radio.dispatchEvent(new Event("change", { bubbles: true }));
+        } else {
+          const same = this.element.querySelectorAll(
+            `input[name="${radio.name}"]`
+          );
+          same.forEach((r) => (r.dataset.wasChecked = "false"));
+          radio.dataset.wasChecked = "true";
+        }
+      });
+    });
   }
 }
