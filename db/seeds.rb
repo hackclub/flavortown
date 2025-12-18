395fd06706d4b2b0b74ff708668b778fd61e77dd
Kartikey Chauhan <me@cskartikey.dev>
2025-12-18 22:11:31 +0530

    feat: implement free stickers

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,14 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-ShopItem::FreeStickers.find_or_create_by!(name: "Free Stickers!") do |item|
+free_stickers = ShopItem::FreeStickers.find_or_create_by!(name: "Free Stickers!") do |item|
   item.one_per_person_ever = true
   item.description = "we'll actually send you these!"
-  item.ticket_cost = 0
+  item.ticket_cost = 10
   downloaded_image = URI.parse("https://hc-cdn.hel1.your-objectstorage.com/s/v3/4367d6fb4a3afa7b2e34ef63c13beaa33922f91b_untitled78_20251203024129__1_.png").open
   item.image.attach(io: downloaded_image, filename: "sticker.png")
 end
+free_stickers.update!(ticket_cost: 10) if free_stickers.ticket_cost != 10
 
 user = User.find_or_create_by!(email: "max@hackclub.com", slack_id: "U09UQ385LSG")
 user.make_super_admin!
