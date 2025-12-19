017c579f809747d5c69477aebcfeee36e076af8b
Max Wofford <max@maxwofford.com>
2025-12-19 15:49:12 -0500

    Add cache counting logic

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,19 @@ module Battlemage
     # MissionControl::Jobs.base_controller_class = "Admin::ApplicationController"
     config.mission_control.jobs.http_basic_auth_enabled = false
 
+    ActiveSupport::Notifications.subscribe("cache_read.active_support") do |*args|
+      event = ActiveSupport::Notifications::Event.new(*args)
+      if event.payload[:hit]
+        Thread.current[:cache_hits] += 1
+      else
+        Thread.current[:cache_misses] += 1
+      end
+    end
+
+    ActiveSupport::Notifications.subscribe("cache_fetch_hit.active_support") do |*args|
+      Thread.current[:cache_hits] += 1
+    end
+
     # what do we want? sessions! when do we want em? now!
     config.session_store :cookie_store,
                          key: "_flavortown_session_v2",
