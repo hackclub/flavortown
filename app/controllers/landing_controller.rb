7e8e609c86b96b3337240846fdace561bd69cd9a
Max Wofford <max@maxwofford.com>
2025-12-19 15:04:17 -0500

    Reapply "Refactor homepage to not hit db (#463)" (#464) (#468)
    * Reapply "Refactor homepage to not hit db (#463)" (#464)
    
    This reverts commit 8a32be72b0031fcd9ca02ae01326db7ff15fbb78.
    
    * Switch to only_path for url generation

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,32 +1,5 @@
 class LandingController < ApplicationController
   def index
-    if current_user
-      steps_left = User::TutorialStep.all.count - current_user.tutorial_steps.count
-      if steps_left > 0
-        redirect_to kitchen_path
-        return
-      else
-        redirect_to projects_path
-        return
-      end
-    end
-    load_prizes
-  end
-
-  def marketing
-    @hide_sidebar = true
-    load_prizes
-    render :index
-  end
-
-  private
-
-  def load_prizes
-    @prizes = Cache::CarouselPrizesJob.perform_now || []
-    if @prizes.any?
-      prize_ids = @prizes.map { |p| p[:id] }
-      records_by_id = ShopItem.with_attached_image.where(id: prize_ids).index_by(&:id)
-      @prizes = @prizes.map { |p| p.merge(record: records_by_id[p[:id]]) }
-    end
+    @prizes = Cache::CarouselPrizesJob.perform_now
   end
 end
--- app/jobs/cache/carousel_prizes_job.rb
+++ app/jobs/cache/carousel_prizes_job.rb
@@ -6,17 +6,65 @@ class Cache::CarouselPrizesJob < ApplicationJob
 
   def perform(force: false)
     Rails.cache.fetch(CACHE_KEY, expires_in: CACHE_DURATION, force: force) do
-      ShopItem.with_attached_image
-              .shown_in_carousel
-              .order(:ticket_cost)
-              .select { |prize| prize.image.attached? }
-              .map do |prize|
-        {
-          id: prize.id,
-          name: prize.name,
-          hours_estimated: prize.hours_estimated
-        }
-      end
+      build_prizes_data
     end
+  end
+
+  private
+
+  def build_prizes_data
+    ShopItem.with_attached_image
+            .shown_in_carousel
+            .order(:ticket_cost)
+            .select { |prize| prize.image.attached? }
+            .map { |prize| build_prize_hash(prize) }
+  end
+
+  def build_prize_hash(prize)
+    sm, md, lg, original = generate_image_urls(prize)
+
+    {
+      id: prize.id,
+      name: prize.name,
+      hours_estimated: prize.hours_estimated,
+      image_src: md || sm || lg || original,
+      image_srcset: build_srcset(sm, md, lg, original)
+    }
+  end
+
+  def generate_image_urls(prize)
+    url_options = { expires_in: CACHE_DURATION + 1.hour }
+
+    [
+      url_for_variant(prize.image.variant(:carousel_sm), url_options),
+      url_for_variant(prize.image.variant(:carousel_md), url_options),
+      url_for_variant(prize.image.variant(:carousel_lg), url_options),
+      url_for_blob(prize.image, url_options)
+    ]
+  end
+
+  def url_for_variant(variant, options)
+    Rails.application.routes.url_helpers.rails_representation_path(
+      variant.processed,
+      only_path: true,
+      **options
+    )
+  end
+
+  def url_for_blob(blob, options)
+    Rails.application.routes.url_helpers.rails_blob_path(
+      blob,
+      only_path: true,
+      **options
+    )
+  end
+
+  def build_srcset(sm, md, lg, original)
+    [
+      ("#{sm} 160w" if sm),
+      ("#{md} 240w" if md),
+      ("#{lg} 360w" if lg),
+      ("#{original} 800w" if original)
+    ].compact.join(", ").presence
   end
 end
