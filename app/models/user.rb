86f5a50a4f0c394f6a6781279d362680e1dfed6a
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-19 13:54:19 -0500

    cache balance!

--- app/models/user.rb
+++ app/models/user.rb
@@ -184,6 +184,10 @@ def all_time_coding_seconds
 
   def balance = ledger_entries.sum(:amount)
 
+  def cached_balance = Rails.cache.fetch(balance_cache_key) { balance }
+  def balance_cache_key = "user/#{id}/sidebar_balance"
+  def invalidate_balance_cache! = Rails.cache.delete(balance_cache_key)
+
   def ban!(reason: nil)
     update!(banned: true, banned_at: Time.current, banned_reason: reason)
     reject_pending_orders!(reason: reason || "User banned")
