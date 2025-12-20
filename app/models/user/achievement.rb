d2bbdd0d5c6c683fa169d61cc1e634f768f9e3a8
Echo <github@3kh0.net>
2025-12-19 20:29:47 -0500

    fix broken annotations

--- app/models/user/achievement.rb
+++ app/models/user/achievement.rb
@@ -7,6 +7,7 @@
 #  id               :bigint           not null, primary key
 #  achievement_slug :string           not null
 #  earned_at        :datetime         not null
+#  notified         :boolean          default(FALSE), not null
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #  user_id          :bigint           not null
