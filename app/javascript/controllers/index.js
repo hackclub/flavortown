ad4b4fca38152ac04a2c2c73db52ddac1514f0bd
Kartikey Chauhan <me@cskartikey.dev>
2025-12-20 20:45:21 +0530

    prefill devlog body and attachment

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -81,3 +81,6 @@ application.register("achievement-highlight", AchievementHighlightController);
 
 import CookieClickerController from "./cookie_clicker_controller";
 application.register("cookie-clicker", CookieClickerController);
+
+import StartDevlogController from "./start_devlog_controller";
+application.register("start-devlog", StartDevlogController);
