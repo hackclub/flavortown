b387afd911f85776604bacc012685ec06b315392
Max Wofford <max@maxwofford.com>
2025-12-20 14:55:12 -0500

    Run lint

--- db/migrate/20251220011331_add_uniqueness_to_posts_postable.rb
+++ db/migrate/20251220011331_add_uniqueness_to_posts_postable.rb
@@ -1,5 +1,5 @@
 class AddUniquenessToPostsPostable < ActiveRecord::Migration[8.1]
   def change
-    add_index :posts, [:postable_type, :postable_id], unique: true
+    add_index :posts, [ :postable_type, :postable_id ], unique: true
   end
 end
