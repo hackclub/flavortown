01b3991031e0f6078b83f21d877321db6d7156e0
Cyteon <129582290+cyteon@users.noreply.github.com>
2025-12-21 09:35:24 +0000

    redirect from / and hidden login to /kitchen

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,5 +2,13 @@ class LandingController < ApplicationController
   def index
     @prizes = Cache::CarouselPrizesJob.fetch
     @hide_sidebar = true
+
+    def index
+      if current_user
+        redirect_to kitchen_path
+      else
+        render :index
+      end
+    end
   end
 end
