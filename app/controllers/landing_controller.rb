5aebe20ed85c07efb978750281f220530ec88917
Cyteon <129582290+cyteon@users.noreply.github.com>
2025-12-21 11:05:22 +0100

    Redirect to kitchen from / or login if you are logged in (#532)
    * redirect from / and hidden login to /kitchen
    
    * fix minor typo

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,5 +2,11 @@ class LandingController < ApplicationController
   def index
     @prizes = Cache::CarouselPrizesJob.fetch
     @hide_sidebar = true
+
+    if current_user
+      redirect_to kitchen_path
+    else
+      render :index
+    end
   end
 end
