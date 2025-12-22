54ba2105a8b288a03204186d6e544a0f7b286af2
Neon <neon@saahild.com>
2025-12-21 20:51:03 -0500

    fix some shop issues (#551)
    * Update shop_controller.rb
    
    * Filter shop items to only enabled ones in order
    
    Change shop_item retrieval to only include enabled items.
    
    * fix: lint
    
    * fix: ok ac fixed now
    
    * fix: #543
    
    * fix: #550
    
    * lint

--- app/controllers/api/v1/store_controller.rb
+++ app/controllers/api/v1/store_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::StoreController < ApplicationController
   include ApiAuthenticatable
 
   def index
-    @items = ShopItem.all
+    @items = ShopItem.enabled
   end
 
   def show
-    @item = ShopItem.find_by!(id: params[:id])
+    @item = ShopItem.enabled.find_by!(id: params[:id])
   rescue ActiveRecord::RecordNotFound
     render json: { error: "Item not found" }, status: :not_found
   end
