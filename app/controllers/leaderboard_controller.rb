38865afe2b7eb3fc9567fe8e5ed0827aab6b2a82
Cyteon <129582290+cyteon@users.noreply.github.com>
2025-12-22 12:02:35 +0000

    add pagination

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,5 +1,6 @@
 class LeaderboardController < ApplicationController
   def index
-    @users = User.where(leaderboard_optin: true)
+    sorted_users = User.where(leaderboard_optin: true).sort_by { |u| -u.cached_balance }
+    @pagy, @users = pagy(:offset, sorted_users, limit: 10)
   end
 end
