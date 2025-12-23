a49e5a27349fbd5611360a01ec38e61eaadf9662
Cyteon <129582290+cyteon@users.noreply.github.com>
2025-12-23 11:29:43 +0000

    add docs base

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,6 +1,41 @@
 class Api::V1::ProjectsController < ApplicationController
   include ApiAuthenticatable
 
+  class_attribute :url_params, default: {}
+  class_attribute :response, default: {}
+
+  self.url_params = {
+    index: {
+      page: { type: Integer, desc: "Page number for pagination", required: false },
+    }
+  }
+
+  self.response = {
+    index: [
+      {
+        id: Integer,
+        title: String,
+        description: String,
+        repo_url: String,
+        demo_url: String,
+        readme_url: String,
+        created_at: String,
+        updated_at: String
+      }
+    ],
+    
+    show: {
+      id: Integer,
+      title: String,
+      description: String,
+      repo_url: String,
+      demo_url: String,
+      readme_url: String,
+      created_at: String,
+      updated_at: String
+    }
+  }
+
   def index
     @pagy, @projects = pagy(Project.where(deleted_at: nil), items: 100)
   end
