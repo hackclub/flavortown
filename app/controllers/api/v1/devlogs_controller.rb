e0d2071ff907126d71b8940a1c931e3b495e5f9c
Cyteon <129582290+cyteon@users.noreply.github.com>
2025-12-23 12:13:49 +0000

    docs for rest of routes

--- app/controllers/api/v1/devlogs_controller.rb
+++ app/controllers/api/v1/devlogs_controller.rb
@@ -1,6 +1,41 @@
 class Api::V1::DevlogsController < ApplicationController
   include ApiAuthenticatable
 
+  class_attribute :url_params, default: {}
+  class_attribute :response, default: {}
+
+  self.url_params = {
+    index: {
+      page: { type: Integer, desc: "Page number for pagination", required: false }
+    }
+  }
+
+  self.response = {
+    index: [
+      {
+        id: Integer,
+        body: String,
+        comments_count: Integer,
+        duration_seconds: Integer,
+        likes_count: Integer,
+        scrapbook_url: String,
+        created_at: String,
+        updated_at: String
+      }
+    ],
+
+    show: {
+      id: Integer,
+      body: String,
+      comments_count: Integer,
+      duration_seconds: Integer,
+      likes_count: Integer,
+      scrapbook_url: String,
+      created_at: String,
+      updated_at: String
+    }
+  }
+  
   def index
     @pagy, @devlogs = pagy(
       Post::Devlog
--- app/controllers/api/v1/store_controller.rb
+++ app/controllers/api/v1/store_controller.rb
@@ -1,6 +1,94 @@
 class Api::V1::StoreController < ApplicationController
   include ApiAuthenticatable
 
+  class_attribute :response, default: {}
+
+  self.response = {
+    index: [
+      {
+        id: Integer,
+        name: String,
+        description: String,
+        old_prices: Array,
+        limited: "Boolean",
+        stock: Integer,
+        type: String,
+        show_in_carousel: "Boolean",
+        accessory_tag: String,
+        agh_contents: Array,
+        attached_shop_item_ids: Array,
+        buyable_by_self: "Boolean",
+        long_description: String,
+        max_qty: Integer,
+        one_per_person_ever: "Boolean",
+        sale_percentage: Integer,
+        image_url: String,
+
+        enabled: {
+          au: "Boolean",
+          ca: "Boolean",
+          eu: "Boolean",
+          in: "Boolean",
+          uk: "Boolean",
+          us: "Boolean",
+          xx: "Boolean"
+        },
+
+        ticket_cost: {
+          base_cost: Integer,
+          au: Integer,
+          ca: Integer,
+          eu: Integer,
+          in: Integer,
+          uk: Integer,
+          us: Integer,
+          xx: Integer
+        }
+      }
+    ],
+
+    show: {
+      id: Integer,
+      name: String,
+      description: String,
+      old_prices: Array,
+      limited: "Boolean",
+      stock: Integer,
+      type: String,
+      show_in_carousel: "Boolean",
+      accessory_tag: String,
+      agh_contents: Array,
+      attached_shop_item_ids: Array,
+      buyable_by_self: "Boolean",
+      long_description: String,
+      max_qty: Integer,
+      one_per_person_ever: "Boolean",
+      sale_percentage: Integer,
+      image_url: String,
+
+      enabled: {
+        au: "Boolean",
+        ca: "Boolean",
+        eu: "Boolean",
+        in: "Boolean",
+        uk: "Boolean",
+        us: "Boolean",
+        xx: "Boolean"
+      },
+
+      ticket_cost: {
+        base_cost: Integer,
+        au: Integer,
+        ca: Integer,
+        eu: Integer,
+        in: Integer,
+        uk: Integer,
+        us: Integer,
+        xx: Integer
+      }
+    }
+  }
+
   def index
     @items = ShopItem.enabled
   end
