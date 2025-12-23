437535c758590e034eb1d8497e7c6bd922972194
Max Wofford <max@maxwofford.com>
2025-12-22 20:51:50 -0500

    Add reason & link on ledger view (#590)

---
+++ app/helpers/ledger_entries_helper.rb
@@ -0,0 +1,20 @@
+module LedgerEntriesHelper
+  def ledger_entry_reason_with_link(entry)
+    case entry.ledgerable
+    when User::Achievement
+      achievement = entry.ledgerable.achievement
+      link_to entry.reason, my_achievements_path(highlight: achievement.slug), data: { turbo_frame: "_top" }
+    when Post::ShipEvent
+      project = entry.ledgerable.post&.project
+      if project
+        link_to entry.reason, project_path(project), data: { turbo_frame: "_top" }
+      else
+        entry.reason
+      end
+    when ShopOrder
+      link_to entry.reason, shop_my_orders_path, data: { turbo_frame: "_top" }
+    else
+      entry.reason
+    end
+  end
+end
