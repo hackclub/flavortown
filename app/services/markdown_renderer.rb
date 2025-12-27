2f716ce11fba68ded639188113df94a6d9222da7
Cyteon <129582290+cyteon@users.noreply.github.com>
2025-12-27 16:01:53 +0000

    implement a lot of markdown styles

--- app/services/markdown_renderer.rb
+++ app/services/markdown_renderer.rb
@@ -1,22 +1,35 @@
 class MarkdownRenderer
     def self.render(text)
         html = get_markdown(text)
-        sanitised = ActionController::Base.helpers.sanitize(html)
+
+        sanitised = ActionController::Base.helpers.sanitize(
+            html,
+            tags: ActionView::Base.sanitized_allowed_tags + ["u"]
+        )
+
         doc = Nokogiri::HTML::DocumentFragment.parse(sanitised)
+
         doc.css("a").each do |link|
           link["target"] = "_blank"
           link["rel"] = "noopener noreferrer"
         end
+
         doc.to_html
     end
 
     private
 
     def self.get_markdown(text)
-        html = Commonmarker.to_html(
+        Commonmarker.to_html(
             text,
             options: {
-                parse: { smart: true }
+                parse: { smart: true },
+                extension: { 
+                    strikethrough: true,
+                    underline: true,
+                    table: true,
+                },
+                render: { unsafe: true } # this should be fine as we sanitize later
             }
         )
     end
