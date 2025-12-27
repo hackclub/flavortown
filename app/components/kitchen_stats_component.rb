51ac466e4868325e12c2c697f01f6546d12aee3f
Echo <github@3kh0.net>
2025-12-27 22:20:32 +0000

    move rankings and awards to sidebar (#621)
    * move rankings and awards to sidebar
    
    * move stats to kitchen

---
+++ app/components/kitchen_stats_component.rb
@@ -0,0 +1,85 @@
+class KitchenStatsComponent < ApplicationComponent
+  def initialize(user:)
+    @user = user
+  end
+
+  def view_template
+    div(class: "kitchen-stats") do
+      h2(class: "kitchen-stats__title") { "Your Progress" }
+      div(class: "kitchen-stats__grid") do
+        render_achievements_card
+        render_leaderboard_card
+      end
+    end
+  end
+
+  private
+
+  def render_achievements_card
+    countable = Achievement.countable_for_user(@user)
+    earned_count = countable.count { |a| a.earned_by?(@user) }
+    total_count = countable.count
+
+    div(class: "state-card state-card--neutral kitchen-stats-card") do
+      div(class: "kitchen-stats-card__content") do
+        div(class: "state-card__title") { "Achievements" }
+        div(class: "kitchen-stats-card__stat") do
+          span(class: "kitchen-stats-card__count") { earned_count.to_s }
+          span(class: "kitchen-stats-card__total") { " / #{total_count}" }
+        end
+        div(class: "state-card__description") do
+          if earned_count.zero?
+            "Start earning achievements!"
+          elsif earned_count == total_count
+            "All achievements unlocked!"
+          else
+            "#{((earned_count.to_f / total_count) * 100).round}% collected"
+          end
+        end
+      end
+      div(class: "state-card__cta") do
+        a(href: helpers.my_achievements_path, class: "btn btn--borderless btn--bg_yellow") do
+          span { "View Achievements" }
+        end
+      end
+    end
+  end
+
+  def render_leaderboard_card
+    rank = calculate_rank
+    balance = @user.cached_balance
+
+    div(class: "state-card state-card--neutral kitchen-stats-card") do
+      div(class: "kitchen-stats-card__content") do
+        div(class: "state-card__title") { "Leaderboard" }
+        div(class: "kitchen-stats-card__stat") do
+          if @user.leaderboard_optin? && rank
+            span(class: "kitchen-stats-card__rank") { "##{rank}" }
+          else
+            span(class: "kitchen-stats-card__rank kitchen-stats-card__rank--unranked") { "Unranked" }
+          end
+        end
+        div(class: "state-card__description") do
+          if @user.leaderboard_optin?
+            "#{balance} 🍪 earned"
+          else
+            "Opt in via settings to rank"
+          end
+        end
+      end
+      div(class: "state-card__cta") do
+        a(href: helpers.leaderboard_path, class: "btn btn--borderless btn--bg_yellow") do
+          span { "View Leaderboard" }
+        end
+      end
+    end
+  end
+
+  def calculate_rank
+    return nil unless @user.leaderboard_optin?
+
+    User.where(leaderboard_optin: true)
+        .where("cached_balance > ?", @user.cached_balance)
+        .count + 1
+  end
+end
