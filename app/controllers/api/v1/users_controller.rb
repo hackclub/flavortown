746449cd1299716deb5988430052c2f111789b52
Eli Ozcan <62805599+elijah629@users.noreply.github.com>
2025-12-28 23:48:39 +0000

    fix: use limit instead of items for pagy (#686)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -41,7 +41,7 @@ class Api::V1::UsersController < Api::BaseController
   }
 
   def index
-    @pagy, @users = pagy(User.includes(:projects).all, page: params[:page], items: 100)
+    @pagy, @users = pagy(User.includes(:projects).all, page: params[:page], limit: 100)
   end
 
   def show
