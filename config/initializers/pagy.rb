a8cbc096fd14cbaaac87a134f4dd7af6a90f4993
Max Wofford <max@maxwofford.com>
2025-12-28 22:38:41 -0500

    New higher default limits on pagination of endpoints (#698)
    * New higher default limits on pagination of endpoints
    
    * Use AR joins for devlogs
    
    * Simplify rack attack defaults
    
    * Remove outdated ratelimit comments in API
    
    * Routes order

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -3,4 +3,5 @@
 # Pagy initializer file (43.x)
 # See https://ddnexus.github.io/pagy/resources/initializer/
 
-Pagy.options[:limit] = 25
+Pagy.options[:limit] = 100
+Pagy.options[:client_max_limit] = 1000
