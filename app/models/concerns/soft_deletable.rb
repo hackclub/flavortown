e1b4cea01c7464894e65ddefbe13bb7244c74c05
Max Wofford <max@maxwofford.com>
2025-12-30 18:51:04 -0500

    Fix typo!

--- app/models/concerns/soft_deletable.rb
+++ app/models/concerns/soft_deletable.rb
@@ -4,7 +4,7 @@ module SoftDeletable
   # Recommend also indexing that column.
 
   included do
-    scope :with_deleted, -> { unscoped.where(deleted_at: nil) }
+    scope :with_deleted, -> { unscoped(where: :deleted_at) }
     scope :deleted, -> { where.not(deleted_at: nil) }
     scope :not_deleted, -> { where(deleted_at: nil) }
     default_scope { not_deleted }
