e545188b62148f8f689419d07aa04fd6d042355e
Max Wofford <max@maxwofford.com>
2026-01-01 15:31:32 -0500

    Denormalize project.duration_seconds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_01_174716) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_01_202509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -365,6 +365,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_01_174716) do
     t.text "demo_url"
     t.text "description"
     t.integer "devlogs_count", default: 0, null: false
+    t.integer "duration_seconds", default: 0, null: false
     t.string "fire_letter_id"
     t.datetime "marked_fire_at"
     t.bigint "marked_fire_by_id"
