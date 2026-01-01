c0cfd3dea518e07ad70961c71bf833ae7279177d
Max Wofford <max@maxwofford.com>
2026-01-01 12:57:09 -0500

    Add soft_deletable concern (#726)
    * Add soft_deletable concern
    
    * Devlogs + projects are SoftDeletable
    
    * Remove unused scope
    
    * Fix double negative in gallery controller
    
    * Fix typo!

---
+++ app/models/concerns/soft_deletable.rb
@@ -0,0 +1,12 @@
+module SoftDeletable
+  extend ActiveSupport::Concern
+  # This relies on a `deleted_at` datetime column being present in the model's table.
+  # Recommend also indexing that column.
+
+  included do
+    scope :with_deleted, -> { unscoped(where: :deleted_at) }
+    scope :deleted, -> { where.not(deleted_at: nil) }
+    scope :not_deleted, -> { where(deleted_at: nil) }
+    default_scope { not_deleted }
+  end
+end
