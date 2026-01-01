hackclub/flavortown

commit b0602de362bcf4974e36775b9a731e916c5b1583

Simplify explore/devlog lookup (#727)
* Simplify explore/devlog lookup

* Simplify explore/project lookup

app/controllers/explore_controller.rb
@@ -2,17 +2,9 @@ class ExploreController < ApplicationController
   VARIANTS = %i[devlog fire certified ship].freeze
 
   def index
-    # Get non-tutorial devlog IDs, filtering out deleted ones for regular users
-    devlog_scope = Post::Devlog.where(tutorial: false)
-    devlog_scope = devlog_scope.with_deleted if current_user&.can_see_deleted_devlogs?
-
-    non_tutorial_devlog_ids = devlog_scope.select(:id)
-
-    scope = Post.includes(:user, :project, postable: { attachments_attachments: :blob, likes: [] })
-                .where(postable_type: "Post::Devlog")
-                .where("posts.postable_id::bigint IN (?)", non_tutorial_devlog_ids)
-                .where.not(user_id: current_user&.id)
-                .order(created_at: :desc)
+    scope = Post::Devlog.includes(:project, :user, attachments_attachments: :blob)
+                        .where(tutorial: false)
+                        .where.not(user: current_user&.id)
 
     @pagy, @devlogs = pagy(scope)
 
@@ -39,9 +31,9 @@ class ExploreController < ApplicationController
 
   def gallery
     scope = Project.includes(banner_attachment: :blob)
-                    .where(tutorial: false)
-                    .where.not(id: current_user&.projects&.pluck(:id) || [])
-                    .order(created_at: :desc)
+                   .where(tutorial: false)
+                   .excluding_member(current_user)
+                   .order(created_at: :desc)
 
     @pagy, @projects = pagy(scope)
 

app/models/project.rb
@@ -47,6 +47,9 @@ class Project < ApplicationRecord
       "Minecraft Mods", "Hardware", "Android App", "iOS App", "Other"
     ].freeze
 
+    scope :excluding_member, ->(user) {
+        user ? where.not(id: user.projects) : all
+    }
     scope :fire, -> { where.not(marked_fire_at: nil) }
 
     belongs_to :marked_fire_by, class_name: "User", optional: true

commit 370315586a298244aa853632b2c4fe14c5ae9d82

Fix for filtering explore page on user (broke in #727)

app/controllers/explore_controller.rb
@@ -2,9 +2,10 @@ class ExploreController < ApplicationController
   VARIANTS = %i[devlog fire certified ship].freeze
 
   def index
-    scope = Post::Devlog.includes(:project, :user, attachments_attachments: :blob)
+    scope = Post::Devlog.includes(:post, attachments_attachments: :blob)
+                        .joins(:post)
                         .where(tutorial: false)
-                        .where.not(user: current_user&.id)
+                        .where.not(posts: { user_id: current_user&.id })
 
     @pagy, @devlogs = pagy(scope)
 