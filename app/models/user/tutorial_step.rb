ee0ba2c770942b0ae7b56475a26da1e6ed124e9f
Max Wofford <max@maxwofford.com>
2026-01-07 19:47:37 +0100

    Clarify posting introduction for tutorial

--- app/models/user/tutorial_step.rb
+++ app/models/user/tutorial_step.rb
@@ -43,7 +43,7 @@ class User
           verb: :post),
       new(slug: :setup_slack,
           name: "Join Slack",
-          description: "connect with the community",
+          description: "post in #flavortown-introductions",
           icon: "slack",
           link: ->(_) { "https://hackclub.slack.com" }),
       new(slug: :free_stickers,
