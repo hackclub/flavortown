dd8450674f55f75b1e068617754962e50418ff85
Mahad Kalam <mahadkalam1234@gmail.com>
2026-01-26 18:41:03 +0000

    annotate the models

--- test/models/cookie_transfer_test.rb
+++ test/models/cookie_transfer_test.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: cookie_transfers
+#
+#  id           :bigint           not null, primary key
+#  amount       :integer          not null
+#  note         :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  recipient_id :bigint           not null
+#  sender_id    :bigint           not null
+#
+# Indexes
+#
+#  index_cookie_transfers_on_recipient_id  (recipient_id)
+#  index_cookie_transfers_on_sender_id     (sender_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (recipient_id => users.id)
+#  fk_rails_...  (sender_id => users.id)
+#
 require "test_helper"
 
 class CookieTransferTest < ActiveSupport::TestCase
