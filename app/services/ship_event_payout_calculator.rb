19a3dd62c208ea6cb40aaadb0c842ade88e53fc6
Neon <neon@saahild.com>
2026-01-27 19:24:22 -0500

    fix: run every 6h,,, (#1139)

--- app/services/ship_event_payout_calculator.rb
+++ app/services/ship_event_payout_calculator.rb
@@ -107,6 +107,11 @@ class ShipEventPayoutCalculator
   def notify_vote_deficit(user, votes_needed)
     return unless user.slack_id.present?
 
+    cache_key = "vote_deficit_notified:#{@ship_event.id}"
+    return if Rails.cache.exist?(cache_key)
+
+    Rails.cache.write(cache_key, true, expires_in: 6.hours)
+
     project = @ship_event.post&.project
     project_title = project&.title
 
