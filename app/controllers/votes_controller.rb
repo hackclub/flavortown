d1fed5dd7c5da0b4a58140c2a211ed4a4a6eceee
Shreyas Deo <93237883+deoshreyas@users.noreply.github.com>
2026-01-30 15:10:00 +0530

    Patch voting bug

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -11,6 +11,12 @@ class VotesController < ApplicationController
     @ship_event = VoteMatchmaker.new(current_user, user_agent: request.user_agent).next_ship_event
     return redirect_to root_path, notice: "No more projects to vote on!" unless @ship_event
 
+    @suggestion_token = VoteSuggestionToken.issue(
+      user: current_user,
+      ship_event: @ship_event,
+      user_agent: request.user_agent
+    )
+
     @vote = Vote.new(ship_event: @ship_event, project: @ship_event.post.project)
     @project = @ship_event.post.project
     @posts = @project.posts.where("created_at <= ?", @ship_event.post.created_at)
@@ -20,7 +26,28 @@ class VotesController < ApplicationController
 
   def create
     authorize :vote
+
+    ship_event_id = VoteSuggestionToken.verify(
+      params[:suggestion_token],
+      user: current_user,
+      user_agent: request.user_agent
+    )
+
+    unless ship_event_id
+      return redirect_to(new_vote_path, alert: "Invalid or expired vote session. Please try again.")
+    end
+
+    ship_event = VoteableShipEventsQuery.call(user: current_user, user_agent: request.user_agent)
+      .includes(post: :project)
+      .find_by(id: ship_event_id)
+
+    unless ship_event&.post&.project
+      return redirect_to(new_vote_path, alert: "That project is no longer available for voting. Please try again.")
+    end
+
     @vote = current_user.votes.build(vote_params)
+    @vote.ship_event = ship_event
+    @vote.project = ship_event.post.project
 
     if @vote.save
       share_vote_to_slack(@vote) if current_user.send_votes_to_slack
@@ -44,8 +71,13 @@ class VotesController < ApplicationController
   end
 
   def vote_params
-    params.require(:vote).permit(:ship_event_id, :project_id, :reason,
-      :demo_url_clicked, :repo_url_clicked, :time_taken_to_vote, *Vote.score_columns)
+    params.require(:vote).permit(
+      :reason,
+      :demo_url_clicked,
+      :repo_url_clicked,
+      :time_taken_to_vote,
+      *Vote.score_columns
+    )
   end
 
   def share_vote_to_slack(vote)
