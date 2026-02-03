c5e2df003a021311c7c141d5be5ae6ba6de1471e
Shreyas Deo <93237883+deoshreyas@users.noreply.github.com>
2026-02-03 22:32:18 +0530

    Add shop suggestion box (#1242)
    * Add shop suggestion box
    
    * Add link to shop suggestions in main admin page
    
    * Add link to the shop suggestion box

---
+++ app/controllers/shop_suggestions_controller.rb
@@ -0,0 +1,28 @@
+class ShopSuggestionsController < ApplicationController
+  before_action :require_login
+
+  def create
+    if current_user.has_dismissed?("shop_suggestion_box") || !Flipper.enabled?(:shop_suggestion_box, current_user)
+      redirect_to shop_path, alert: "Suggestion box is not available."
+      return
+    end
+
+    @suggestion = current_user.shop_suggestions.build(suggestion_params)
+
+    if @suggestion.save
+      redirect_to shop_path, notice: "Thank you for your suggestion!"
+    else
+      redirect_to shop_path, alert: @suggestion.errors.full_messages.to_sentence
+    end
+  end
+
+  private
+
+  def suggestion_params
+    params.require(:shop_suggestion).permit(:item, :explanation, :link)
+  end
+
+  def require_login
+    redirect_to root_path, alert: "Please log in first" and return unless current_user
+  end
+end
---
+++ app/models/shop_suggestion.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: shop_suggestions
+#
+#  id          :bigint           not null, primary key
+#  explanation :text
+#  item        :text
+#  link        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_shop_suggestions_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class ShopSuggestion < ApplicationRecord
+  belongs_to :user
+
+  validates :item, presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :explanation, presence: true, length: { minimum: 10, maximum: 10000 }
+  validates :link, allow_blank: true, length: { maximum: 2000 }, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]) }
+end
