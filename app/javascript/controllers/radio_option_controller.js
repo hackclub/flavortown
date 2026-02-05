1e2d61564987483d6fd3f66998bfae5666f49487
ascpixi <44982772+ascpixi@users.noreply.github.com>
2026-02-05 00:56:44 +0000

    refactor: remove unused code from `radio_option_controller.js`

--- app/javascript/controllers/radio_option_controller.js
+++ app/javascript/controllers/radio_option_controller.js
@@ -1,8 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static values = { name: String, value: String };
-
   connect() {
     this.element.addEventListener("click", this.select.bind(this));
   }
@@ -24,24 +22,5 @@ export default class extends Controller {
           hiddenInput.disabled = !isSelected;
         }
       });
-
-    this.updateHiddenInput();
-  }
-
-  updateHiddenInput() {
-    const name = this.nameValue;
-    let input = document.querySelector(
-      `input[type="hidden"][name="${name}"][data-radio-group-input]`,
-    );
-
-    if (!input) {
-      input = document.createElement("input");
-      input.type = "hidden";
-      input.name = name;
-      input.dataset.radioGroupInput = "true";
-      this.element.closest("form")?.appendChild(input);
-    }
-
-    input.value = this.valueValue;
   }
 }
