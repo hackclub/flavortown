e2819e3e5ecfb5522f6511500d3726eb1c64eb46
Max Wofford <max@maxwofford.com>
2026-02-06 13:38:29 -0500

    Dynamic sitemap.xml

---
+++ app/jobs/cache/sitemap_job.rb
@@ -0,0 +1,32 @@
+class Cache::SitemapJob < ApplicationJob
+  queue_as :literally_whenever
+
+  CACHE_KEY = "sitemap_xml"
+  CACHE_DURATION = 1.hour
+
+  def self.fetch(force: false)
+    Rails.cache.fetch(CACHE_KEY, expires_in: CACHE_DURATION, force: force) do
+      new.send(:build_sitemap)
+    end
+  end
+
+  def perform(force: false)
+    Rails.cache.fetch(CACHE_KEY, expires_in: CACHE_DURATION, force: force) do
+      build_sitemap
+    end
+  end
+
+  private
+
+  def build_sitemap
+    projects = Project.where.not(ship_status: "draft").select(:id, :updated_at)
+    users = User.where(banned: false).select(:id, :updated_at)
+    sidequests = Sidequest.select(:id, :slug, :updated_at)
+
+    ApplicationController.render(
+      template: "sitemaps/index",
+      formats: [ :xml ],
+      assigns: { projects: projects, users: users, sidequests: sidequests }
+    )
+  end
+end
