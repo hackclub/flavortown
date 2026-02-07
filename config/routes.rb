0df81bb4c8ab816cc81bc269a1e4da28011e2961
Neon <neon@saahild.com>
2026-02-07 12:19:21 -0500

    update: #1173 (#1314)
    * update: #1173
    
    * update: partial wip
    
    * update: make button js lowk request re cert when rejected
    
    * update: insta request re cert from slack dm

--- config/routes.rb
+++ config/routes.rb
@@ -313,6 +313,7 @@ Rails.application.routes.draw do
       post :follow
       delete :unfollow
       post :resend_webhook
+      get :confirm_recertification
       post :request_recertification
     end
   end
