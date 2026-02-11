997aabe2da241e82405a211dcab3b39daaf78891
Neon <neon@saahild.com>
2026-02-11 15:18:24 +0000

    annotate

--- app/models/disco_recommendation.rb
+++ app/models/disco_recommendation.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: disco_recommendations
+#
+#  id           :bigint           not null, primary key
+#  context      :string
+#  item_type    :string
+#  score        :float
+#  subject_type :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  item_id      :bigint
+#  subject_id   :bigint
+#
+# Indexes
+#
+#  index_disco_recommendations_on_item     (item_type,item_id)
+#  index_disco_recommendations_on_subject  (subject_type,subject_id)
+#
 class DiscoRecommendation < ApplicationRecord
   belongs_to :subject, polymorphic: true
   belongs_to :item, polymorphic: true
