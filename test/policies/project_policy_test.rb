d91e82a22e3e57aa7e7144f0094c52052a8c686d
Dhamari Trice-Hanson <39872667+dhamariT@users.noreply.github.com>
2026-02-13 18:18:56 -0500

    Fix project policy test to run without fixtures

--- test/policies/project_policy_test.rb
+++ test/policies/project_policy_test.rb
@@ -1,20 +1,39 @@
 require "test_helper"
 
-class ProjectPolicyTest < ActiveSupport::TestCase
-  test "project certifier can confirm and request recertification without membership" do
-    user = users(:two)
-    user.granted_roles = [ "project_certifier" ]
+class ProjectPolicyTest < Minitest::Test
+  MembershipsStub = Struct.new(:is_member) do
+    def exists?(project:)
+      is_member
+    end
+  end
 
-    policy = ProjectPolicy.new(user, projects(:one))
+  UserStub = Struct.new(:is_certifier, :is_member) do
+    def project_certifier?
+      is_certifier
+    end
+
+    def memberships
+      MembershipsStub.new(is_member)
+    end
+  end
+
+  def test_project_certifier_can_confirm_and_request_recertification_without_membership
+    user = UserStub.new(true, false)
+    project = Object.new
+
+    policy = ProjectPolicy.new(user, project)
 
     assert policy.confirm_recertification?
     assert policy.request_recertification?
   end
 
-  test "non-member without certifier role cannot confirm or request recertification" do
-    policy = ProjectPolicy.new(users(:two), projects(:one))
+  def test_non_member_without_certifier_role_cannot_confirm_or_request_recertification
+    user = UserStub.new(false, false)
+    project = Object.new
 
-    assert_not policy.confirm_recertification?
-    assert_not policy.request_recertification?
+    policy = ProjectPolicy.new(user, project)
+
+    refute policy.confirm_recertification?
+    refute policy.request_recertification?
   end
 end
