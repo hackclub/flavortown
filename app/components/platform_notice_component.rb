20503779b2958a8cb9c9b04683551eb7eb8dea44
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-02-23 22:29:11 +0000

    Add styles to make it fit in the kitchen

--- app/components/platform_notice_component.rb
+++ app/components/platform_notice_component.rb
@@ -1,7 +1,9 @@
 class PlatformNoticeComponent < ViewComponent::Base
-  attr_reader :heading_element
+  attr_reader :heading_element, :with_margin
 
-  def initialize(heading_element: "h2")
+  def initialize(heading_element: "h2", with_margin: false)
+    @message_link = "https://hackclub.slack.com/archives/C09MATJJZ5J/p1771879301452079"
     @heading_element = heading_element
+    @with_margin = with_margin
   end
 end
