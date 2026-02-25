ab0112d54d2a28c9b933209ca85b5cbfa5ad0045
Kartikey Chauhan <me@cskartikey.dev>
2026-02-25 19:51:18 +0530

    refactor: remove dead code

--- app/components/project_show_card_component.rb
+++ app/components/project_show_card_component.rb
@@ -19,10 +19,6 @@ class ProjectShowCardComponent < ViewComponent::Base
     project.banner.variant(:card)
   end
 
-  #   def followers_count
-  #     project.memberships_count
-  #   end
-
   def has_any_links?
     project.demo_url.present? || project.repo_url.present? || project.readme_url.present?
   end
@@ -33,11 +29,6 @@ class ProjectShowCardComponent < ViewComponent::Base
     else
       project.followers.size
     end
-  end
-
-  def owner_display_name
-    owner = project.memberships.includes(:user).owner.first&.user
-    owner&.display_name || project.users.first&.display_name || "Unknown"
   end
 
   def byline_text
