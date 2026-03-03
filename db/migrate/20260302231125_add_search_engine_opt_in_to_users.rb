a0cf20fa2c2ab96c2dcf6895c6d93bee47686c1e
Shreyas Deo <93237883+deoshreyas@users.noreply.github.com>
2026-03-03 08:40:14 +0530

    make it opt out by default

--- db/migrate/20260302231125_add_search_engine_opt_in_to_users.rb
+++ db/migrate/20260302231125_add_search_engine_opt_in_to_users.rb
@@ -1,5 +1,5 @@
 class AddSearchEngineOptInToUsers < ActiveRecord::Migration[8.1]
   def change
-    add_column :users, :search_engine_opt_in, :boolean, default: false, null: false
+    add_column :users, :search_engine_opt_in, :boolean, default: true, null: false
   end
 end
