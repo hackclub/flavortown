5bc2efdb242b441ae411069d7c8c31fd721f0b50
Shreyas Deo <93237883+deoshreyas@users.noreply.github.com>
2026-03-09 18:48:05 +0530

    Support vibecheck improvements (#1665)
    * Make support vibe check a recurring job
    
    * Link to sample questions in current concerns on super mega dash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_05_062122) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_09_121342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -701,6 +701,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_05_062122) do
   end
 
   create_table "support_vibes", force: :cascade do |t|
+    t.jsonb "concern_message_links"
     t.jsonb "concerns", default: []
     t.datetime "created_at", null: false
     t.jsonb "notable_quotes", default: []
