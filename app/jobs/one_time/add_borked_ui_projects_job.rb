d03e164638c76f9f07799747cc8b9dd007d275a1
Shreyas Deo <93237883+deoshreyas@users.noreply.github.com>
2026-03-21 07:38:29 +0530

    Backfill sidequests db for hackazine and borked ui jam (#1844)
    * Attempt to fix hackazine sidequest job
    
    * Add borked ui jam sidequest projects to db
    
    * Change date on homepage
    
    * commit annotations
    
    * fix failing checks
    
    * Revert "fix failing checks"
    
    This reverts commit c07bc6dcc7a102d0ee5b0127860ecf1e74e8ffec.
    
    * Revert "commit annotations"
    
    This reverts commit 9d14e399f7601abcb988bee5917d7fa79395e516.
    
    * fix checks

--- app/jobs/one_time/add_borked_ui_projects_job.rb
+++ app/jobs/one_time/add_borked_ui_projects_job.rb
@@ -1,12 +1,12 @@
-class OneTime::AddHackazineProjectsJob < ApplicationJob
+class OneTime::AddBorkedUiProjectsJob < ApplicationJob
     queue_as :literally_whenever
 
     def perform
-        sidequest = Sidequest.find_or_create_by!(slug: "the_hackazine") do |record|
-            record.title = "Hackazine"
+        sidequest = Sidequest.find_or_create_by!(slug: "borked_ui_jam") do |record|
+            record.title = "Borked UI Jam"
         end
 
-        project_ids = [ 2935, 140, 7256, 6494, 2381, 1865, 3984, 781 ]
+        project_ids = [ 7386, 7898, 7484, 8300, 7867, 8332, 7448, 10092, 10135, 8258, 10246, 10484, 10373 ]
 
         project_ids.each do |project_id|
             project = Project.find_by(id: project_id)
