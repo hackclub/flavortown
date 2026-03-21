dfd7c35b1209868b5c95b96410d5c5da1bbd16ec
Neon <neon@saahild.com>
2026-03-21 11:59:13 -0400

    update: api endpoints include banned and shadow banned for admins + its own sepr endpoint (#1775)

--- config/routes.rb
+++ config/routes.rb
@@ -158,6 +158,9 @@ Rails.application.routes.draw do
 
     namespace :v1 do
       resources :projects, only: [ :index, :show, :create, :update ] do
+        member do
+          get :ban_status
+        end
         collection do
           get :random
           get :search
