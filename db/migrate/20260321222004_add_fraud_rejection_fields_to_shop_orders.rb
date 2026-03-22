02ef01986d7136270b76280ea71635c5bc0dcf01
Neon <neon@saahild.com>
2026-03-22 00:20:30 +0000

    fix: e

--- db/migrate/20260321222004_add_fraud_rejection_fields_to_shop_orders.rb
+++ db/migrate/20260321222004_add_fraud_rejection_fields_to_shop_orders.rb
@@ -3,6 +3,5 @@ class AddFraudRejectionFieldsToShopOrders < ActiveRecord::Migration[8.1]
     add_column :shop_orders, :internal_rejection_reason, :text
     add_column :shop_orders, :joe_case_url, :string
     add_column :shop_orders, :fraud_related_project_id, :bigint
-    add_foreign_key :shop_orders, :projects, column: :fraud_related_project_id, on_delete: :nullify
   end
 end
