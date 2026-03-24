c3ee58b0737f5dd32ae222d1330fb1a413b581bd
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-03-24 15:15:35 -0600

    feat: improved music player
    Improved the music player by adding a separate pause/play button and the ability to switch songs

--- app/javascript/controllers/music_player_controller.js
+++ app/javascript/controllers/music_player_controller.js
@@ -1,48 +1,96 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static values = { audioUrl: String };
+  static values = { audioUrls: Array, defaultIndex: { type: Number, default: 0 } };
 
   connect() {
-    this.audio = new Audio(this.audioUrlValue);
-    this.audio.loop = true;
-    this.audio.volume = 0.5;
-
+    this.currentIndex = this.defaultIndexValue;
     this.isPlaying = false;
+    this.volume = 0.5;
+    this.audioContext = null;
+    this.gainNode = null;
+    this.sourceNode = null;
+    this.buffer = null;
   }
 
-  togglePlay() {
+  async togglePlay() {
     if (this.isPlaying) {
-      this.audio.pause();
-      this.isPlaying = false;
+      this.#pause();
     } else {
-      this.audio.play();
-      this.isPlaying = true;
+      await this.#play();
     }
     this.#toggleIcons();
   }
 
   changeVolume(event) {
-    this.audio.volume = event.target.value;
+    this.volume = parseFloat(event.target.value);
+    if (this.gainNode) this.gainNode.gain.value = this.volume;
+  }
+
+  async prevTrack() {
+    this.currentIndex = (this.currentIndex - 1 + this.audioUrlsValue.length) % this.audioUrlsValue.length;
+    await this.#switchTrack();
+  }
+
+  async nextTrack() {
+    this.currentIndex = (this.currentIndex + 1) % this.audioUrlsValue.length;
+    await this.#switchTrack();
   }
 
   disconnect() {
-    if (this.audio) {
-      this.audio.pause();
-      this.audio = null;
+    this.#pause();
+    this.audioContext?.close();
+    this.audioContext = null;
+  }
+
+  async #play() {
+    if (!this.audioContext) {
+      this.audioContext = new AudioContext();
+      this.gainNode = this.audioContext.createGain();
+      this.gainNode.gain.value = this.volume;
+      this.gainNode.connect(this.audioContext.destination);
+    }
+
+    await this.audioContext.resume();
+
+    if (!this.buffer) await this.#loadBuffer();
+
+    this.sourceNode = this.audioContext.createBufferSource();
+    this.sourceNode.buffer = this.buffer;
+    this.sourceNode.loop = true;
+    this.sourceNode.connect(this.gainNode);
+    this.sourceNode.start(0);
+
+    this.isPlaying = true;
+  }
+
+  #pause() {
+    if (this.sourceNode) {
+      this.sourceNode.onended = null;
+      this.sourceNode.stop();
+      this.sourceNode = null;
+    }
+    this.isPlaying = false;
+  }
+
+  async #switchTrack() {
+    const wasPlaying = this.isPlaying;
+    this.#pause();
+    this.buffer = null;
+    if (wasPlaying) {
+      await this.#play();
+      this.#toggleIcons();
     }
   }
 
-  #toggleIcons() {
-    const playIcon = this.element.querySelector(".music-player__icon--play");
-    const pauseIcon = this.element.querySelector(".music-player__icon--pause");
+  async #loadBuffer() {
+    const response = await fetch(this.audioUrlsValue[this.currentIndex]);
+    const arrayBuffer = await response.arrayBuffer();
+    this.buffer = await this.audioContext.decodeAudioData(arrayBuffer);
+  }
 
-    if (this.isPlaying) {
-      playIcon.hidden = true;
-      pauseIcon.hidden = false;
-    } else {
-      playIcon.hidden = false;
-      pauseIcon.hidden = true;
-    }
+  #toggleIcons() {
+    this.element.querySelectorAll(".music-player__icon--play").forEach(el => el.hidden = this.isPlaying);
+    this.element.querySelectorAll(".music-player__icon--pause").forEach(el => el.hidden = !this.isPlaying);
   }
 }
