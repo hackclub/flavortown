e4c9bb1d1d1e631ef652a0576ff2e2c3614cff8c
Echo <github@3kh0.net>
2026-03-25 18:34:34 -0400

    fix funky ci indexing

--- db/schema.rb
+++ db/schema.rb
@@ -842,6 +842,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_25_221946) do
     t.boolean "voting_locked", default: false, null: false
     t.boolean "ysws_eligible", default: false, null: false
     t.index ["airtable_record_id"], name: "index_users_on_airtable_record_id", unique: true
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true
     t.index ["email"], name: "index_users_on_email"
     t.index ["magic_link_token"], name: "index_users_on_magic_link_token", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
