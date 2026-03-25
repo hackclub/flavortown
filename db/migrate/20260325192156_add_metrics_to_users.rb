aa3d320aef4d0b56f4e1a7caa40135de2cca5262
vandorena <alexvd@hackclub.com>
2026-03-25 15:32:34 -0400

    updating migration

--- db/migrate/20260325192156_add_metrics_to_users.rb
+++ db/migrate/20260325192156_add_metrics_to_users.rb
@@ -2,9 +2,8 @@ class AddMetricsToUsers < ActiveRecord::Migration[8.1]
   def change
     add_column :users, :flavortown_message_count_14d, :integer
     add_column :users, :flavortown_support_message_count_14d, :integer
-    add_column :users, :projects_count, :integer
+    # projects_count already exists - skipping
     add_column :users, :projects_shipped_count, :integer
     add_column :users, :slack_messages_updated_at, :datetime
-    add_column :users, :metrics_synced_at, :datetime
   end
 end
