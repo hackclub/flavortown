7108f1ef17f15309f1a01d792d990867b2d77427
Obay M. Rashad <102649548+ObayM@users.noreply.github.com>
2026-03-28 03:24:09 +0200

    fixed the seeds.rb and clarified smth in the faq (#1977)

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,9 +81,9 @@ Sidequest.find_or_create_by!(slug: "converge") do |sq|
   sq.description = "Build a Slack or Discord bot that does something useful or creative. Ship it on Flavortown and submit it to unlock Converge prizes in the shop!"
 end
 
-lockin_sq = Sidequest.find_or_create_by!(slug: "lockin")
-lockin_sq.update!(
+Sidequest.find_or_create_by!(
   title: "Lock in",
+  slug: "lockin",
   description: "Work 10 hrs a week for 4 weeks without missing an hour. You get 120 cookies & unlock some shop items.",
   expires_at: Date.new(2026, 4, 30)
 )
