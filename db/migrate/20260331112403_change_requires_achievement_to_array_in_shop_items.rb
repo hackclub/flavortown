a6bcf24dc03ad08ef99060c476d261fcf03dc033
Echo <github@3kh0.net>
2026-03-31 13:43:09 -0400

    remove no op migration

--- db/migrate/20260331112403_change_requires_achievement_to_array_in_shop_items.rb
+++
@@ -1,6 +0,0 @@
-class ChangeRequiresAchievementToArrayInShopItems < ActiveRecord::Migration[8.1]
-  # no-op on purpose.
-  # https://hackclub.slack.com/archives/C09KCFWQSKE/p1774975606120579
-  def change
-  end
-end
