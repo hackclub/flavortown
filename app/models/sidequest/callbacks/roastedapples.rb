a10f22cbc765918c4e6da209f8756784db82cc53
Eobam <elliottlieberman123@gmail.com>
2026-04-08 10:20:58 -0500

    Fix roastedapples.rb in Zeitwark checks

--- app/models/sidequest/callbacks/roastedapples.rb
+++ app/models/sidequest/callbacks/roastedapples.rb
@@ -1,26 +1,23 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
-module Sidequest:Callbacks
-    class Roastedapples
-        class << self
-            def on_approve(entry)
-                user = entry.project_owner 
-                return unless user
+module Sidequest::Callbacks
+  class Roastedapples
+    class << self
+      def on_approve(entry)
+        user = entry.project_owner
+        return unless user
 
+        user.award_achievement!(:sidequest_roastedapples)
+        SendSlackDmJob.perform_later(
+          user.id,
+          nil,
+          blocks_path: "notifications/sidequests/roastedapples_approved",
+          locals: { entry: entry, user: user }
+        )
+      end
 
-                user.award_achievement!(:sidequest_roastedapples)
-                SendSlackDmJob.perform_later(
-                    user.id,
-                    nil,
-                    blocks_path: "notifications/sidequests/roastedapples_approved",
-                    locals: { entry: entry, user: user }
-                  )
-                
-                end
-
-
-                def on_reject(entry)
-                    user = entry.project_owner
+      def on_reject(entry)
+        user = entry.project_owner
         return unless user
 
         SendSlackDmJob.perform_later(
