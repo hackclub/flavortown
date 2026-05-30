0cc59bcf2bf406b714b082078bd450d87b3c9ef6
Max Wofford <max@maxwofford.com>
2026-05-30 14:08:22 -0400

    Read app host w/ fallback in production asset config (#2296)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = "https://flavortown.hackclub.com"
+  config.asset_host = "https://#{ENV.fetch("APP_HOST", "flavortown.hackclub.com")}"
 
   # Proxy mode for CDN
   config.active_storage.resolve_model_to_route = :rails_storage_proxy
